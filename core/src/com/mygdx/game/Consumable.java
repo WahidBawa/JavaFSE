3d601d555ac014ef2b65b64d4f6513449ad8d335
Wahid Bawa <wahidbawa10@gmail.com>
2019-06-14 13:26:21 -0400

    commented Consumables

--- core/src/com/mygdx/game/Consumable.java
+++ core/src/com/mygdx/game/Consumable.java
@@ -1,20 +1,23 @@
+/*
+    Author: Wahid Bawa & Andi Morarescu
+    Purpose: stores the data and the methods for all consumable items
+ */
 package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Consumable extends Item {
     public Consumable(String name, String type, int value, boolean stackable) {
-        super(name, type, value, stackable);
+        super(name, type, value, stackable); // initializes using the Item class constructor
     }
 
     @Override
-    public void use(Player player) {
-        System.out.println("THIS IS A " + name.toUpperCase());
-        player.stats.put((type.equals("health") ? "Health" : "Mana"), (Integer) Main.player.stats.get((type.equals("health") ? "Health" : "Mana")) + value);
+    public void use(Player player) { // uses the item
+        player.stats.put((type.equals("health") ? "Health" : "Mana"), (Integer) Main.player.stats.get((type.equals("health") ? "Health" : "Mana")) + value); // adds to mana or health depending on the type of item
     }
 
     @Override
     public Sprite getImg() {
         return super.getImg();
-    }
+    } // returns image
 }
